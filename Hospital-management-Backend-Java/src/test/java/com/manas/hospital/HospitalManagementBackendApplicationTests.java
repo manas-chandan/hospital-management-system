e17236b1e6507c173e2f8a082e2e1960e7d05d2e
Manasbarik-holcim <manas.barik.ext@holcim.com>
2026-09-22 01:02:33 +0530

    Initial Spring Boot backend setup

---
+++ Hospital-management-Backend-Java/src/test/java/com/manas/hospital/HospitalManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manas.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
